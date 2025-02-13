a649ea4288a568ba82fd0370b98fb5b64126fc95
Ashabh <mohdashabh@gmail.com>
2025-02-13 13:58:34 +0530

    Creatin Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { Route, Routes } from 'react-router-dom'
-import Home from './Pages/Home'
-import Login from './Pages/Login'
-import Register from './Pages/Register'
-import CreatePost from './Pages/CreatePost'
-import PostDetails from './Pages/PostDetails'
-import EditPost from './Pages/EditPost'
-import MyBlogs from './Pages/MyBlogs'
-import Profile from './Pages/Profile'
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import CreatePost from "./pages/CreatePost";
+import PostDetails from "./pages/PostDetails";
+import EditPost from "./pages/EditPost";
+import MyBlogs from "./pages/MyBlogs";
+import Profile from "./pages/Profile";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-     <Routes>
-
-      <Route exact path='/' element={Home} />
-      <Route exact path='/login' element={Login} />
-      <Route exact path='/register' element={Register} />
-      <Route exact path='/write' element={CreatePost} />
-      <Route exact path='/Post/post/:id' element={PostDetails} />
-      <Route exact path='/edit/:id' element={EditPost} />
-      <Route exact path='/myBlogs/:id' element={MyBlogs} />
-      <Route exact path='/profile' element={Profile} />
-
-      
-     </Routes>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/write" element={<CreatePost />} />
+        <Route path="/Post/post/:id" element={<PostDetails />} />
+        <Route path="/edit/:id" element={<EditPost />} />
+        <Route path="/myBlogs/:id" element={<MyBlogs />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
     </>
-  )
+  );
 }
 
-
-export default App
+export default App;
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <div>Navbar</div>
-  )
+    <div>
+      <div className="flex items-center justify-between px-6 md:px-[200px] py-4 bg-purple-800 text-indigo-100">
+        <h1 className="text-lg md:text-x1 font-extrabold">
+          <Link to={"/"}>Blogesh</Link>
+        </h1>
+      </div>
+      Navbar
+    </div>
+  );
 }
 
-export default Navbar
+export default Navbar;
