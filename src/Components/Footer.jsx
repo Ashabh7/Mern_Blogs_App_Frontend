6c6375a32b42972a885e3a6d42bb49baa3f54f69
Ashabh <mohdashabh@gmail.com>
2025-03-03 14:16:33 +0530

    7/ 16:22

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -2,7 +2,39 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <div>
+      <div className='mt-8 w-full bg-black px-8 md:px-[300px] flex justify-center md:flex-row flex-col space-y-6 md:space-y-0
+      items-start justify-between sm:justify-center md:justify-between text-sm md:text-md py-8'>
+
+        <div className='flex flex-col text-white'>
+          <p> Featured Blogs </p>
+          <p> Most Viewed</p>
+          <p>Readers choice</p>
+
+        </div>
+         
+        <div className='flex flex-col text-white'>
+          <p> Forum </p>
+          <p> Support </p>
+          <p> Recent Posts </p>
+
+        </div>
+
+        <div className='flex flex-col text-white'>
+          <p> Privacy Policy </p>
+          <p> About Us </p>
+          <p> Terms & Conditions </p>
+
+        </div>
+
+
+
+      </div>
+
+      <p className='py-2 pb-6 text-center text-black bg-blacktext-sm'>All Rights Reserved @Blogesh 2025 </p>
+
+
+    </div>
   )
 }
 
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,7 +1,46 @@
 import React from 'react'
 import Navbar from '../Components/Navbar'
+import axios from 'axios'
+import HomePost from '../Components/HomePost'
+import Footer from '../Components/Footer'
+import { URL } from '../url'
+import { useEffect,useContext,useState } from 'react'
+import { Link,useLocation } from 'react-router-dom'
+import Loader from '../Components/Loader'
+import { UserContext } from '../context/UserContext'
 
 function Home() {
+
+  const {search} = useLocation()
+  const [posts,setPosts] = useState([])
+  const [noResults,setNoResults] = useState(false)
+  const [Loader,setLoader] = useState(false)
+  const {user} = useContext(UserContext)
+  const [cat,setCat] = useState([])
+  const [filterData, setFilterData] = useState([])
+  
+
+  const fetchPosts = async () => {
+    setLoader(true)
+    try {
+      const res = await axios.get(URL + "/api.posts/" + search)
+      setPosts(res.data)
+      setFilterData(res.data)
+      let cata = res.data.map((item) => {return item.categories})     //Creating Category Filter
+      let sets = new Set()
+      cata.forEach((category) => {
+        if(cata.length > 0) sets.add(catas)
+      })
+      
+
+    } catch (err) {
+      console.log(err);
+      setLoader(true)
+      
+    }
+  }
+
+
   return (
     <div>
       <Navbar/>
