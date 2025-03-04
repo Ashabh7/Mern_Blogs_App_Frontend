1ac0921958f2a6d01fa003c94adcd16e570b13ce
Ashabh <mohdashabh@gmail.com>
2025-03-04 14:11:34 +0530

    7/56

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Footer() {
   return (
     <div>
-      <div className='mt-8 w-full bg-black px-8 md:px-[300px] flex justify-center md:flex-row flex-col space-y-6 md:space-y-0
+      <div className='mt-8 w-full bg-black px-8 md:px-[300px] flex md:flex-row flex-col space-y-6 md:space-y-0
       items-start justify-between sm:justify-center md:justify-between text-sm md:text-md py-8'>
 
         <div className='flex flex-col text-white'>
