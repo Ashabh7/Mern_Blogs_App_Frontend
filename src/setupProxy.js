30187f8d0cf4ca60a3d2579c73f3f626ad5d4fcf
Ashabh <mohdashabh@gmail.com>
2025-03-07 14:01:07 +0530

    CreatePost

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,15 +1,15 @@
-const {createProxyMiddleware} = require('http-proxy-middleware')
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function (root) {
     root.use(
-        '/api', // Adjust The Path You Want to Proxy 
-        createProxyMiddleware({
-            target: 'http:localhost:8000', // Specify The Address of Your Backend Server 
-            changeOrigin: true,
-            secure: false, // Set to false If Your Backend Doesn't Use HTTPS
-            headers: {
-                'Access-Control-Allow-Origin': 'http://localhost:3000'// Adjust The React App's Origin
-            }  
-        })
-    )
-}
+    '/api', // Adjust the path you want to proxy
+    createProxyMiddleware({
+      target: 'http://localhost:8000', // Specify the address of your backend server
+      changeOrigin: true,
+      secure: false, // Set to false if your backend doesn't use HTTPS
+      headers: {
+        'Access-Control-Allow-Origin': 'http://localhost:3000', // Adjust the React app's origin
+      },
+    })
+  );
+};
