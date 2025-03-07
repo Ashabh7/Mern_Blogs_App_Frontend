bf6276bd101c5373e1e59a997426eae45e44acb2
Ashabh <mohdashabh@gmail.com>
2025-03-07 14:06:53 +0530

    Home

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,123 +1,103 @@
-import React from 'react'
-import Navbar from '../Components/Navbar'
-import axios from 'axios'
-import HomePost from '../Components/HomePost'
-import Footer from '../Components/Footer'
-import { URL } from '../url'
-import { useEffect,useContext,useState } from 'react'
-import { Link,useLocation } from 'react-router-dom'
+import axios from "axios"
+import Footer from "../Components/Footer"
+import HomePosts from "../components/HomePosts"
+import Navbar from "../Components/Navbar"
+import { IF, URL } from "../url"
+import { useContext, useEffect, useState } from "react"
+import { Link, useLocation } from "react-router-dom"
 import Loader from '../Components/Loader'
-import { UserContext } from '../context/UserContext'
+import { UserContext } from "../context/UserContext"
+ 
 
-function Home() {
-
-  const {search} = useLocation()
-  const [posts,setPosts] = useState([])
-  const [noResults,setNoResults] = useState(false)
-  const [loader,setLoader] = useState(false)
-  const {user} = useContext(UserContext)
-  const [cat,setCat] = useState([])
-  const [filterData, setFilterData] = useState([])
+const Home = () => {
   
+  const {search}=useLocation()
+  // console.log(search)
+  const [posts,setPosts]=useState([])
+  const [noResults,setNoResults]=useState(false)
+  const [loader,setLoader]=useState(false)
+  const {user}=useContext(UserContext)
+  const [cat,setCat]=useState([])
+  const [filter,setFilter]=useState(false)
+  const [filterData,setFilterData] = useState([])
+  // console.log(user)
 
-  const fetchPosts = async () => {
+  const fetchPosts=async()=>{
     setLoader(true)
-    try {
-      const res = await axios.get(URL + "/api/posts/" + search)
+    try{
+      const res=await axios.get(URL+"/api/posts/"+search)
+      // console.log(res.data)
       setPosts(res.data)
       setFilterData(res.data)
-      let cata = res.data.map((item) => {return item.categories})     //Creating Category Filter
-      let sets = new Set()
-      cata.forEach((category) => {
-        category?.forEach((catas) => {
-
-          if(cata.length > 0) sets.add(catas)
-
+      let cata=res.data.map((item)=>{return item.categories})
+      let sets=new Set()
+      cata.forEach((category)=>{
+        category?.forEach((catas)=>{
+          if(catas.length>0)
+          sets.add(catas)
         })
       })
       setCat(Array.from(sets))
 
-      console.log(res.data);
-
-      if(res.data.length === 0) {
+      console.log(res.data)
+      if(res.data.length===0){
         setNoResults(true)
       }
       else{
         setNoResults(false)
       }
-      setNoResults(false)
-
+      setLoader(false)
       
-      
-
-    } catch (err) {
-      console.log(err);
+    }
+    catch(err){
+      console.log(err)
       setLoader(true)
-      
     }
   }
 
-  useEffect(() => {
+  useEffect(()=>{
     fetchPosts()
-  }, [search])
 
-  const fillterData = (filterData) => {
-    let newpost = posts.filter((pos) => {
-      return pos?.categories.includes(fillterData)
-    })
+  },[search])
+
+  const fillterData=(filterDatas)=>{
+    let newpost=posts.filter((pos)=>{return pos?.categories.includes(filterDatas)})
     setFilterData(newpost)
   }
 
-
   return (
+    
+    <>
+    <Navbar/>
     <div>
-      <Navbar/>
+    <div className="  flex flex-wrap   " >
+      <div className=" p-3 m-5 flex flex-wrap justify-center  " >
 
-      <div>
-        <div className='flex flex-wrap'>
-          <div className='p-3 m-5 flex flex-wrap justify-center'>
+      {cat.length && cat?.map((catgory)=>{
+        return <button className="p-3 m-5  h-[90px] w-[150px] border text-lg font-semibold  bg-white hover:shadow-blue-200 shadow shadow-black " onClick={()=>fillterData(catgory)}>{catgory}</button>
+      })}
+      </div>
+    </div>
+<div className=" flex flex-wrap   w-[95%] justify-center ">
+        {loader?<div className="h-screen flex justify-center items-center">
+          <Loader/></div>:!noResults?
+        filterData.map((post)=>(
+          <div className=" flex flex-wrap m-2 sm:w-[35vh] lg:w-[45vh] md:w-[50vh] ">
+          <Link to={user?`/posts/post/${post._id}`:"/login"}>
+          <HomePosts key={post._id} post={post}/>
+          </Link>
 
-            {
-              cat.length && cat?.map((category) => {
-                return <button className='p-3 m-5 h-[90px] w-[150px] border text-lg font-semibold bg-white
-                 hover:shadow-blue-200 shadow shadow-black' onClick={() => fillterData(category)}>
-                  {category}
-                </button>
-              })
-            }
+        
 
           </div>
-
-        </div>
-
-        <div className=' flex flex-wrap w-[95%] justify-center'>
-          {
-            loader ? <div className='h-screen flex justify-center items-center'>
-              <Loader/>
-            </div>:!noResults ? 
-            filterData.map((post) => 
-              (
-                <div className='flex flex-wrap m-2 sm:w-[35vw] lg:w-[45vw] md:w-[50vw]'>
-                  <Link to={user ? '/posts/post/${post._id' : "/login"}>
-                  <HomePost key={post._id} post={post} />
-                  </Link>
-
-                </div>
-            )
-          ) : <h3 className='text-center font-bold mt-16'>
-            No Posts Aavilable
-          </h3>
-          }
-
-        </div>
-
-      </div>
-
-      <Footer/>
-
-
-
+          
+          
+        )):<h3 className="text-center font-bold mt-16">No posts available</h3>}
     </div>
+    </div>
+    <Footer/>
+    </>
+    
   )
 }
 
